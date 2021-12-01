a15e2a45b4d58e125259de2aca5644425c45653b
Russel <russelmlove@gmail.com>
2021-12-01 03:18:13 -0500

    Switched from StackPane to VBox

--- src/com/russel/games/entry/MainEntryPoint.java
+++ src/com/russel/games/entry/MainEntryPoint.java
@@ -1,14 +1,15 @@
 package com.russel.games.entry;
 
+import java.util.Stack;
 
-import com.russel.games.processor.GameManager;
 import com.russel.games.model.Dictionary;
+import com.russel.games.processor.GameManager;
 import com.russel.games.processor.WordView;
 
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -23,9 +24,10 @@ import javafx.stage.Stage;
 public class MainEntryPoint extends Application{
 	Dictionary dictionary;
 	
-	private StackPane root;
+	private VBox root;
 	private long startTime;
-	
+	private Stack<String> wordStack;
+	private int index = 0;
 	/**
 	 * starting point of the application
 	 * @param args
@@ -41,39 +43,71 @@ public class MainEntryPoint extends Application{
 		//get the scene setup
 		Scene scene = new Scene(setUp());
 		
+		//setup the initial words
+		setInitialWords();
+		
+		showNextWord();
+		
 		//get the scene to listen for keyboard events 
         scene.setOnKeyPressed(e -> onKeyPress(e.getCode().toString()));
 
         primaryStage.setTitle("SpeedTyper!");
         
+        
         //display the scene on the stage
         primaryStage.setScene(scene);
         primaryStage.show();
+       
+	}
+	
+	/**
+	 * sets up the word stack with initial values
+	 */
+	private void setInitialWords() {
+		wordStack = new Stack<String>();
+		for(int i = 0; i < 2; i++) wordStack.push(" ");
+		for(int i = 0; i < 2; i++) wordStack.push((dictionary.getRandomWord()));
+		for(int i = 0; i < wordStack.size(); i++) root.getChildren().add(new WordView(wordStack.get(i)));
+		System.out.println(wordStack);
+		//root.getChildren().add(new WordView(wordStack.get(0)));
+		//root.getChildren().add(new WordView(wordStack.get(1)));
+		
 	}
 	
 	
 	private Parent setUp() {
-		root = new StackPane();
-		root.setPrefSize(900, 700);
-		showNextWord();
+		//root = new StackPane();
+		root = new VBox();
+		root.setPrefSize(900, 700);		
 		return root;
 	}
 	
 	/**
 	 * grabs a random word from the dictionary and prepares it to display it on the screen.
+	 * TODO - grab word from the stack instead of getting a new word from the dictionary
 	 */
     private void showNextWord() {
-        String word = dictionary.getRandomWord();
-
+        //String word = dictionary.getRandomWord();
+    	
+    	//get the word at index3, then adjust the stack so the words move up
+    	
+    	String word = wordStack.get(3);
+    	//wordStack.pop();
+    	//wordStack.remove(0);
+    	
+    	//wordStack.push(dictionary.getRandomWord());
+    	
         WordView view = new WordView(word);
+        
+        root.getChildren().add(view);
+       // root.getChildren().setAll(view);
 
-        root.getChildren().setAll(view);
 
         startTime = System.nanoTime();
     }
     
     private void onKeyPress(String letter) {
-        WordView view = (WordView) root.getChildren().get(0);
+        WordView view = (WordView) root.getChildren().get(2);
         view.handleKeyPress(letter);
 
         /*
@@ -82,11 +116,30 @@ public class MainEntryPoint extends Application{
          * each word the user types should get pushed up and the further it gets pushed, the smaller it should get until its off screen and it should get deleted
          */
         if (view.isFinished()) {
+        	wordStack.remove(0);
+        	
+        	wordStack.push(dictionary.getRandomWord());
             long endTime = System.nanoTime() - startTime;
 
             System.out.printf("%.2f sec\n", endTime / 1000000000.0);
-
+            //index++;
+            root.getChildren().remove(0);
+            root.getChildren().get(0).setScaleX(.5);
+            root.getChildren().get(1).setScaleX(.75);
+            root.getChildren().get(2).setScaleX(1);
+            root.getChildren().get(3).setScaleX(.75);
+           // root.getChildren().get(4).setScaleX(.5);
+            
+            root.getChildren().get(0).setScaleY(.5);
+            root.getChildren().get(1).setScaleY(.75);    
+            root.getChildren().get(2).setScaleY(1);
+            root.getChildren().get(3).setScaleY(.75);
+            
+            System.out.println(wordStack);
+           // root.getChildren().get(4).setScaleY(.5);
             showNextWord();
+            root.getChildren().get(4).setScaleX(.5);
+            root.getChildren().get(4).setScaleY(.5);
         }
     }
 
